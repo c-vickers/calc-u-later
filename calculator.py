c02adaf754a20af33a6a4bbea91e00e250a2feb6
Rosie <rosie@riveter16-md.swna.wdpr.disney.com>
2016-04-07 12:06:59 -0700

    Updated Square, Cube, and Modulo Operators

--- calculator.py
+++ calculator.py
@@ -12,18 +12,34 @@ def tokenize_it():
 	while play_calc == 1:
 		user_input = raw_input("> ") 
 		tokens = user_input.split(" ")
+		
 		if tokens[0] == "+":
 			print add(int(tokens[1]),int(tokens[2]))
+		
 		elif tokens[0] == "-":
 			print subtract(int(tokens[1]),int(tokens[2]))
+		
 		elif tokens[0] == "*":
 			print multiply(int(tokens[1]),int(tokens[2]))
+		
 		elif tokens[0] == "/":
 			print divide(float(tokens[1]),float(tokens[2]))
+		
+		elif tokens[0] == "square":
+			print square(int(tokens[1]))
+		
+		elif tokens[0] == "cube":
+			print cube(int(tokens[1]))
+		
 		elif tokens[0] == "pow":
 			print power(int(tokens[1]),int(tokens[2]))
+		
+		elif tokens[0] == "mod":
+			print mod(int(tokens[1]),int(tokens[2]))
+		
 		elif tokens[0] == "q":
 			play_calc = 2
+		
 		else:
 			print "I don't understand that. Please try again."
 			print "If you'd like to exit enter q."
