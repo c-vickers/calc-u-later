9e03aadc5a7c7011545318b79415ae6ecc5c1e60
Rosie <rosie@riveter16-md.swna.wdpr.disney.com>
2016-04-06 17:55:49 -0700

    Updated Calculator Add, Subtract, Power

---
+++ calculator.py
@@ -0,0 +1,31 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+def tokenize_it():
+	play_calc = 1
+	while play_calc == 1:
+		user_input = raw_input("> ") 
+		tokens = user_input.split(" ")
+		if tokens[0] == "+":
+			print add(int(tokens[1]),int(tokens[2]))
+		if tokens[0] == "-":
+			print subtract(int(tokens[1]),int(tokens[2]))
+		elif tokens[0] == "pow":
+			print power(int(tokens[1]),int(tokens[2]))
+		elif tokens[0] == "q":
+			play_calc = 2
+		else:
+			print "I don't understand that. Please try again."
+			print "If you'd like to exit enter q."
+
+
+#while play_calc == 1:
+print "Please only enter two numbers for each arithmetic operation."
+tokenize_it()
+print "Thank you for using this fantastic calculator. Calc-U-Later!"
